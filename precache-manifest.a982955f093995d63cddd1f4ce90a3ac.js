ce41a948208c1728d1c25a11e5d312a4d0f1312c
brookebachman00 <bbachman1994@gmail.com>
2020-10-05 18:53:14 -0600

    Updates

---
+++ precache-manifest.a982955f093995d63cddd1f4ce90a3ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1bb20a4ab59817050dffa4042cebbf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d16aceea481acd722c2",
+    "url": "/static/css/main.602d1b94.chunk.css"
+  },
+  {
+    "revision": "5548d76f35486dbb3342",
+    "url": "/static/js/2.fd415fe6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fd415fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d16aceea481acd722c2",
+    "url": "/static/js/main.d6a9b14c.chunk.js"
+  },
+  {
+    "revision": "5a182da05432246114d1",
+    "url": "/static/js/runtime-main.166752ed.js"
+  }
+]);
